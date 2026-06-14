bbc61477608369336f02a951fc1c66a07ad25ee8
Sara Fontes <sfontes94@gmail.com>
2026-06-14 15:54:28 +0100

    added readme icones

--- src/__main__.py
+++ src/__main__.py
@@ -11,3 +11,19 @@ Run the constrained decoding process.
 Save generated function calls to the output file.
 Coordinate the overall execution flow.
 """
+# No fim do ficheiro temporariamente
+from src.schemas import FunctionDefinition
+
+if __name__ == "__main__":
+    data = {
+        "name": "fn_add_numbers",
+        "description": "Add two numbers.",
+        "parameters": {
+            "a": {"type": "number"},
+            "b": {"type": "number"}
+        },
+        "returns": {"type": "number"}
+    }
+    fn = FunctionDefinition(**data)
+    print(fn.name)
+    print(fn.parameters["a"].type)
--- src/schemas.py
+++ src/schemas.py
@@ -1,4 +1,4 @@
-""""
+"""
 Defines the project's data structures and validation models.
 
 Responsibilities:
@@ -10,3 +10,41 @@ Provide validation and serialization helpers.
 Keep data handling consistent across the project.
 
 """
+from pydantic import BaseModel, field_validator
+
+
+class ParameterSchema(BaseModel):
+    """
+    Represents the type for each parameter on the JSON file:
+    "string", "number", "integer", and "boolean"
+    """
+    type: str
+
+    @field_validator('type')
+    @classmethod
+    def validate_parameter_schema(cls, value: str) -> str:
+        types: list[str] = ["string", "number", "integer", "boolean"]
+        if value not in types:
+            raise ValueError("Invalid type on ParameterSchema.")
+        return value
+
+
+class FunctionDefinition(BaseModel):
+    """
+    Represents each field on JSON:
+    name,description,parameters,returns
+    """
+    name: str
+    description: str
+    parameters: dict[str, ParameterSchema]
+    returns: ParameterSchema
+
+
+class FunctionCall(BaseModel):
+    """
+    Represents the final output,
+    To be written on the JSON file
+    """
+    prompt: str
+    name: str
+    parameters: dict[str, float | str | bool]
